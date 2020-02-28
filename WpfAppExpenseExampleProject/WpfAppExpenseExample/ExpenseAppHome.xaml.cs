e869bfa93fd3d74b79985be8f19ad927dae37479
Yavor Kolev <ygk@mail.bg>
2020-02-28 10:59:08 +0200

    Add files via upload
    Simple WPF C# Education example Visual Studio 2019, No database inline(hard coded) data, standalone Windows application that allows users to view expense reports for selected people.
    Tutorial from link: https://docs.microsoft.com/en-us/dotnet/framework/wpf/getting-started/walkthrough-my-first-wpf-desktop-application

---
+++ WpfAppExpenseExampleProject/WpfAppExpenseExample/ExpenseAppHome.xaml.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfAppExpenseExample
+{
+    /// <summary>
+    /// Interaction logic for ExpenseAppHome.xaml
+    /// </summary>
+    public partial class ExpenseAppHome : Page
+    {
+        public ExpenseAppHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            ExpenseReportPage expenseReportPage = new ExpenseReportPage(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(expenseReportPage);
+        }
+    }
+}
